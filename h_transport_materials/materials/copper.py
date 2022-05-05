7056d4d7787576854085ffb81787bcc9c9ea589c
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-05 21:09:39 +0200

    added range

--- h_transport_materials/materials/copper.py
+++ h_transport_materials/materials/copper.py
@@ -10,8 +10,14 @@ cited_in_database_for_ITER = "cited in Assessment of Database for Interaction of
 # ################# REITER 1996 #############################
 
 reiter_src = "F. Reiter, K. S. Forcey, and G. Gervasini. A compilation of tritium : Material interaction parameters in fusion reactor materials. en. Publications Office of the European Union, July 1996."
-reiter_diffusivity_copper = ArheniusProperty(pre_exp=6.6e-7, act_energy=0.39, source=reiter_src, name="T Reiter (1996)")
-reiter_solubility = ArheniusProperty(pre_exp=3.14e24, act_energy=0.57, source=reiter_src, name="T Reiter (1996)")
+reiter_diffusivity_copper = ArheniusProperty(
+    pre_exp=6.6e-7, act_energy=0.39,
+    range=(470, 1200),
+    source=reiter_src, name="T Reiter (1996)")
+reiter_solubility = ArheniusProperty(
+    pre_exp=3.14e24, act_energy=0.57,
+    range=(470, 1200),
+    source=reiter_src, name="T Reiter (1996)")
 
 copper = Material(D=reiter_diffusivity_copper, S=reiter_solubility, name="copper")
 
