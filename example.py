84f3d337450ce646b61e74a971ee31b3c91aedd0
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-05 21:09:56 +0200

    removed T_bounds

--- example.py
+++ example.py
@@ -12,18 +12,14 @@ with plt.style.context(matplotx.styles.dufte):
 
     plt.sca(axs[0])
     # tungsten
-    T_bounds_W = (300, 1300)
-    plot(frauenfelder_diffusivity, color=grey_W, T_bounds=T_bounds_W)
-    plot(reiter_diffusivity_tungsten, color=grey_W, T_bounds=T_bounds_W)
+    plot(frauenfelder_diffusivity, color=grey_W)
+    # plot(reiter_diffusivity_tungsten, color=grey_W, T_bounds=T_bounds_W)
 
     # copper
-    T_bounds_Cu = (470, 1200)
-    plot(reiter_diffusivity_copper, color=orange_Cu, T_bounds=T_bounds_Cu)
+    plot(reiter_diffusivity_copper, color=orange_Cu)
 
     # cucrzr
-    T_bounds_CuCrZr = (561, 769)
-    plot(serra_diffusivity_h, color=yellow_CuCrZr, T_bounds=T_bounds_CuCrZr)
-    plot(serra_diffusivity_h_eq, color=yellow_CuCrZr, T_bounds=T_bounds_CuCrZr)
+    plot(serra_diffusivity_h, color=yellow_CuCrZr)
 
     plt.yscale("log")
     plt.xlabel("")
@@ -32,17 +28,14 @@ with plt.style.context(matplotx.styles.dufte):
     plt.ylabel("Diffusivity (m$^2$ s$^{-1}$)")
 
     plt.sca(axs[1])
-        # tungsten
-    T_bounds_W = (300, 1300)
-    plot(frauenfelder_solubility, color=grey_W, T_bounds=T_bounds_W)
+    # tungsten
+    plot(frauenfelder_solubility, color=grey_W)
 
     # copper
-    T_bounds_Cu = (470, 1200)
-    plot(reiter_solubility, color=orange_Cu, T_bounds=T_bounds_Cu)
+    plot(reiter_solubility, color=orange_Cu)
 
     # cucrzr
-    T_bounds_CuCrZr = (561, 769)
-    plot(serra_solubility_h_eq, color=yellow_CuCrZr, T_bounds=T_bounds_CuCrZr)
+    plot(serra_solubility_h, color=yellow_CuCrZr)
 
     plt.yscale("log")
 
@@ -50,10 +43,11 @@ with plt.style.context(matplotx.styles.dufte):
     l2, = plt.plot([], [], color=orange_Cu, label="Copper")
     l3, = plt.plot([], [], color=yellow_CuCrZr, label="CuCrZr")
     legend_lines = [l1, l2, l3]
-    legend = plt.legend(legend_lines, [l.get_label() for l in legend_lines])
+    legend = plt.legend(legend_lines, [l.get_label() for l in legend_lines], loc="upper right")
     plt.gca().add_artist(legend)
     # matplotx.line_labels()  # put labels on the right
     line_labels()
+    plt.ylim(top=1e23)
     plt.ylabel("Solubility (m$^{-3}$ Pa$^{-0.5}$)")
     plt.tight_layout()
     plt.show()
