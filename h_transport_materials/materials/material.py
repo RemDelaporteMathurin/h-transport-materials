4b96aa79201148bb66dfbbad57d038649f41948c
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-18 17:46:07 +0200

    added make_festim_material method

--- h_transport_materials/materials/material.py
+++ h_transport_materials/materials/material.py
@@ -1,10 +1,34 @@
-from h_transport_materials import Property
+from h_transport_materials import Property, ArheniusProperty
+
+try:
+    import FESTIM
+except ModuleNotFoundError:
+    print("FESTIM module not found")
+
 
 class Material:
-    def __init__(self, D, S, lambd=None, rho=None, cp=None, name: str=None):
+    def __init__(
+        self,
+        D: ArheniusProperty,
+        S: ArheniusProperty,
+        lambd=None,
+        rho=None,
+        cp=None,
+        name: str = None,
+    ):
         self.D = D
         self.S = S
         self.lambd = lambd
         self.rho = rho
         self.cp = cp
         self.name = name
+
+        self.festim_material = None
+
+    def make_festim_material(self):
+        return FESTIM.Material(
+            D_0=self.D.pre_exp,
+            E_D=self.D.act_energy,
+            S_0=self.S.pre_exp,
+            E_S=self.S.act_energy,
+        )
