fee1b41e9836d861fdcccfc882e03e8bf3035ab4
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-18 17:46:56 +0200

    import

--- h_transport_materials/__init__.py
+++ h_transport_materials/__init__.py
@@ -1,8 +1,8 @@
-
 k_B = 8.617e-5  # eV/K
 Rg = 8.314
 avogadro_nb = 6.022e23
 from .property import Property, ArheniusProperty
 from .materials import *
+from .properties_group import PropertiesGroup, diffusivities, solubilities
 from . import fitting
 from . import plotting
