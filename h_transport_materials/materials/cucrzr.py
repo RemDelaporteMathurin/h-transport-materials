761ec08b29b2a1c9242bb0097518df5bd4b0890c
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-18 21:53:16 +0200

    added cucrzr properties

--- h_transport_materials/materials/cucrzr.py
+++ h_transport_materials/materials/cucrzr.py
@@ -1,4 +1,4 @@
-from h_transport_materials import k_B, Rg, avogadro_nb
+from h_transport_materials import k_B, Rg, avogadro_nb, diffusivities, solubilities
 from h_transport_materials.materials import Material
 from h_transport_materials.property import ArheniusProperty
 
@@ -7,7 +7,7 @@ import numpy as np
 
 anderl_src = "R. A. Anderl et al. 'Deuterium transport in Cu, CuCrZr, and Cu/Be'. In: Journal of Nuclear Materials 266-269 (Mar. 1999), pp. 761–765"
 anderl_recombination = ArheniusProperty(
-    pre_exp=2.9e-14, act_energy=1.92, source=anderl_src, name="Anderl (1999)"
+    pre_exp=2.9e-14, act_energy=1.92, source=anderl_src, name="Anderl (1999)", year=1999, author="anderl", isotope="D"
 )
 
 serra_src = "E Serra and A Perujo. 'Hydrogen and deuterium transport and inventory parameters in a Cu–0.65Cr–0.08Zr alloy for fusion reactor applications'. en. In: Journal of Nuclear Materials 258-263 (Oct. 1998), pp. 1028–1032"
@@ -19,6 +19,9 @@ serra_diffusivity_h_eq = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="H Serra (1998)",
+    author="serra",
+    isotope="H",
+    year=1998
 )
 serra_diffusivity_d_eq = ArheniusProperty(
     pre_exp=4.8e-7,
@@ -26,6 +29,9 @@ serra_diffusivity_d_eq = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="D Serra (1998)",
+    author="serra",
+    isotope="D",
+    year=1998
 )
 serra_diffusivity_T_eq = ArheniusProperty(
     pre_exp=3.07e-7,
@@ -33,6 +39,9 @@ serra_diffusivity_T_eq = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="T Serra (1998)",
+    author="serra",
+    isotope="T",
+    year=1998
 )
 
 serra_solubility_h_eq = ArheniusProperty(
@@ -41,6 +50,9 @@ serra_solubility_h_eq = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="H Serra (1998)",
+    author="serra",
+    isotope="H",
+    year=1998
 )
 serra_solubility_d_eq = ArheniusProperty(
     pre_exp=0.71 * avogadro_nb,
@@ -48,6 +60,9 @@ serra_solubility_d_eq = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="D Serra (1998)",
+    author="serra",
+    isotope="D",
+    year=1998
 )
 serra_solubility_t_eq = ArheniusProperty(
     pre_exp=0.84 * avogadro_nb,
@@ -55,6 +70,9 @@ serra_solubility_t_eq = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="T Serra (1998)",
+    author="serra",
+    isotope="T",
+    year=1998
 )
 
 
@@ -76,6 +94,9 @@ serra_diffusivity_h = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="H Serra (1998) (refitted)",
+    author="serra",
+    isotope="H",
+    year=1998
 )
 
 serra_diffusivity_d = ArheniusProperty(
@@ -84,6 +105,9 @@ serra_diffusivity_d = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="D Serra (1998) (refitted)",
+    author="serra",
+    isotope="D",
+    year=1998
 )
 
 # solubility
@@ -101,6 +125,9 @@ serra_solubility_h = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="H Serra (1998) (refitted)",
+    author="serra",
+    isotope="H",
+    year=1998
 )
 
 serra_solubility_d = ArheniusProperty(
@@ -109,6 +136,9 @@ serra_solubility_d = ArheniusProperty(
     range=(553, 773),
     source=serra_src,
     name="D Serra (1998) (refitted)",
+    author="serra",
+    isotope="D",
+    year=1998
 )
 
 serra_diffusivity_iter = ArheniusProperty(
@@ -116,7 +146,10 @@ serra_diffusivity_iter = ArheniusProperty(
     act_energy=0.418,
     range=(553, 773),
     source=serra_src,
+    author="serra",
     name="T Serra acc. ITER (1998)",
+    isotope="T",
+    year=1998
 )
 
 cucrzr = Material(D=serra_diffusivity_h, S=serra_solubility_h, name="cucrzr")
@@ -128,6 +161,9 @@ nog_diffusivity_cucrzr_t = ArheniusProperty(
     range=(573, 873),
     source="Hydrogen-isotope transport in an ELBRODUR G CuCrZr alloy for nuclear applications in heat sinks, Journal of Nuclear Materials, Volume 473, May 2016, Pages 112–118",
     name="T Noh (2016)",
+    author="noh",
+    isotope="T",
+    year=2016
 )
 
 nog_solubility_cucrzr_t_1 = ArheniusProperty(
@@ -136,6 +172,9 @@ nog_solubility_cucrzr_t_1 = ArheniusProperty(
     range=(573, 873),
     source="Hydrogen-isotope transport in an ELBRODUR G CuCrZr alloy for nuclear applications in heat sinks, Journal of Nuclear Materials, Volume 473, May 2016, Pages 112–118",
     name="T Noh (2016)",
+    author="noh",
+    isotope="T",
+    year=2016
 )
 
 nog_solubility_cucrzr_t_2 = ArheniusProperty(
@@ -144,6 +183,9 @@ nog_solubility_cucrzr_t_2 = ArheniusProperty(
     range=(573, 873),
     source="Hydrogen-isotope transport in an ELBRODUR G CuCrZr alloy for nuclear applications in heat sinks, Journal of Nuclear Materials, Volume 473, May 2016, Pages 112–118",
     name="T Noh (2016)",
+    author="noh",
+    isotope="T",
+    year=2016
 )
 
 # ################# Anderl 1999 #############################
@@ -153,6 +195,9 @@ anderl_diffusivity_cucrzr_d = ArheniusProperty(
     range=(700, 800),
     source="Deuterium transport in Cu, CuCrZr, and Cu/Be, Journal of Nuclear Materials 266-269 (1999) 761-765",
     name="D Anderl (1999)",
+    author="anderl",
+    isotope="D",
+    year=1999
 )
 
 # ################# Penalva 1999 #############################
@@ -162,6 +207,9 @@ penalva_diffusivity_cucrzr_h = ArheniusProperty(
     range=(593, 773),
     source="Interaction of Copper Alloys with Hydrogen, Copper Alloys – Early Applications and Current Performance – Enhancing Processes",
     name="D Penalva (1999)",
+    isotope="D",
+    author="penalva",
+    year=1999
 )
 
 penalva_solubility_cucrzr_h = ArheniusProperty(
@@ -170,4 +218,36 @@ penalva_solubility_cucrzr_h = ArheniusProperty(
     range=(593, 773),
     source="Interaction of Copper Alloys with Hydrogen, Copper Alloys – Early Applications and Current Performance – Enhancing Processes",
     name="D Penalva (1999)",
+    author="penalva",
+    year=1999,
+    isotope="D"
 )
+
+cucrzr_diffusivities = [
+    serra_diffusivity_h_eq,
+    serra_diffusivity_d_eq,
+    serra_diffusivity_T_eq,
+    serra_diffusivity_h,
+    serra_diffusivity_d,
+    serra_diffusivity_iter,
+    nog_diffusivity_cucrzr_t,
+    anderl_diffusivity_cucrzr_d,
+    penalva_diffusivity_cucrzr_h,
+]
+
+cucrzr_solubilities = [
+    serra_solubility_h_eq,
+    serra_solubility_d_eq,
+    serra_solubility_t_eq,
+    serra_solubility_h,
+    serra_solubility_d,
+    nog_solubility_cucrzr_t_1,
+    nog_solubility_cucrzr_t_2,
+    penalva_solubility_cucrzr_h,
+]
+
+for prop in cucrzr_diffusivities + cucrzr_solubilities:
+    prop.material = "cucrzr"
+
+diffusivities.properties += cucrzr_diffusivities
+solubilities.properties += cucrzr_solubilities
