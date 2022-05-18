fe51cbf97de47ddddc5e4d09c015f5d3f9b8c4c6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-18 22:45:35 +0200

    added exclude

--- h_transport_materials/properties_group.py
+++ h_transport_materials/properties_group.py
@@ -5,8 +5,12 @@ class PropertiesGroup:
     def __getitem__(self, item):
         return self.properties[item]
 
-    def filter(self, **kwargs):
+    def filter(self, exclude=False, **kwargs):
         """_summary_
+
+        Args:
+            exclude (bool, optional): if True, the searched
+                keys will be excluded. Defaults to False.
 
         Returns:
             PropertiesGroup or htm.Property: _description_
@@ -16,5 +20,8 @@ class PropertiesGroup:
         for key, value in kwargs.items():
             for prop in list_of_searched_props:
                 if getattr(prop, key) == value:
+                    if not exclude:
+                        list_of_props.properties.append(prop)
+                elif exclude:
                     list_of_props.properties.append(prop)
         return list_of_props
