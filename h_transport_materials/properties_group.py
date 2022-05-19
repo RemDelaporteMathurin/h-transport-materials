801d24eeb8675fbdd4d444114de415181877a8c6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-19 09:17:06 +0200

    added mean method

--- h_transport_materials/properties_group.py
+++ h_transport_materials/properties_group.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from h_transport_materials.fitting import fit_arhenius
+
+
 class PropertiesGroup:
     def __init__(self) -> None:
         self.properties = []
@@ -18,3 +23,22 @@ class PropertiesGroup:
                 if getattr(prop, key) == value:
                     list_of_props.properties.append(prop)
         return list_of_props
+
+    def mean(self, samples_per_line=5):
+        data_T = np.array([])
+        data_y = np.array([])
+        for prop in self.properties:
+            if prop.data_T is not None:
+                prop_T = prop.data_T
+                prop_y = prop.data_y
+            else:
+                T_range = prop.range
+                if prop.range == None:
+                    T_range = (300, 1200)
+                prop_T = np.linspace(T_range[0], T_range[1], num=samples_per_line)
+                prop_y = prop.value(prop_T)
+            data_T = np.concatenate((data_T, prop_T))
+            data_y = np.concatenate((data_y, prop_y))
+
+        pre_exp, act_energy = fit_arhenius(data_y, data_T)
+        return pre_exp, act_energy
