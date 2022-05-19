3a938b924e039c2683a3e4a7dceffa6379ad7f73
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-19 10:45:27 +0200

    added test for value

--- tests/test_arhenius_property.py
+++ tests/test_arhenius_property.py
@@ -1,3 +1,9 @@
+import pytest
+import numpy as np
+
+import h_transport_materials as htm
+
+
 def test_fit():
     pass
 
@@ -10,5 +16,12 @@ def test_act_energy_getter_fit():
     pass
 
 
-def test_value():
-    pass
+@pytest.mark.parametrize(
+    "T,pre_exp,act_energy", [(300, 3, 0.2), (205, 6, 0.8), (600, 1e5, 1.2)]
+)
+def test_value(T, pre_exp, act_energy):
+    my_prop = htm.ArheniusProperty(pre_exp=pre_exp, act_energy=act_energy)
+
+    computed_value = my_prop.value(T=T)
+    expected_value = pre_exp * np.exp(-act_energy / htm.k_B / T)
+    assert expected_value == computed_value
