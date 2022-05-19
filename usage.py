c2918c27804c876be020745d05e96bc670d5b204
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-19 09:24:31 +0200

    added mean value to usage

--- usage.py
+++ usage.py
@@ -1,16 +1,19 @@
 import h_transport_materials as htm
 import matplotlib.pyplot as plt
-import numpy as np
 
-my_D = htm.diffusivities.filter(material="copper")
+my_D = htm.diffusivities.filter(material="tungsten")
 
 mean_D_0, mean_E_D = my_D.mean()
 
 for prop in my_D:
-    htm.plotting.plot(prop)
+    htm.plotting.plot(prop, alpha=0.5)
 
 D_mean = htm.ArheniusProperty(mean_D_0, mean_E_D)
 htm.plotting.plot(D_mean, color="black", linewidth=3)
 
+x_annotation = 0.0034
+plt.annotate("mean value", (x_annotation, D_mean.value(T=1 / x_annotation)))
+
+plt.ylabel("Diffusivity (m$^2$ s$^{-1}$)")
 plt.yscale("log")
 plt.show()
