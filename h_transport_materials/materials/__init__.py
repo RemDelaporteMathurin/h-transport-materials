d8818f48022379b0a99f502509f9c99693c288fa
jhdark <james.dark.95@gmail.com>
2022-06-01 22:10:37 +0200

    lipb integrated and plot tested

--- h_transport_materials/materials/__init__.py
+++ h_transport_materials/materials/__init__.py
@@ -3,3 +3,4 @@ from .copper import copper_diffusivities, copper_solubilities
 from .cucrzr import cucrzr_diffusivities, cucrzr_solubilities
 from .tungsten import tungsten_diffusivities, tungsten_solubilities
 from .flinak import flinak_diffusivities, flinak_solubilities
+from .lipb import lipb_diffusivities, lipb_solubilities
