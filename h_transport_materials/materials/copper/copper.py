f9b686b7675ecbbdc99ea5e181ad3d77ee6b34bc
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-06-05 12:11:04 +0200

    copper references from bibtex

--- h_transport_materials/materials/copper/copper.py
+++ h_transport_materials/materials/copper/copper.py
@@ -9,12 +9,11 @@ cited_in_database_for_ITER = "cited in Assessment of Database for Interaction of
 
 # ################# REITER 1996 #############################
 
-reiter_src = "F. Reiter, K. S. Forcey, and G. Gervasini. A compilation of tritium : Material interaction parameters in fusion reactor materials. en. Publications Office of the European Union, July 1996."
 reiter_diffusivity_copper = ArrheniusProperty(
     pre_exp=6.6e-7,
     act_energy=0.39,
     range=(470, 1200),
-    source=reiter_src,
+    source="reiter_compilation_1996",
     name="T Reiter (1996)",
     year=1996,
     isotope="T",
@@ -24,7 +23,7 @@ reiter_solubility_copper = Solubility(
     pre_exp=3.14e24,
     act_energy=0.57,
     range=(470, 1200),
-    source=reiter_src,
+    source="reiter_compilation_1996",
     name="T Reiter (1996)",
     year=1996,
     isotope="T",
@@ -35,12 +34,11 @@ reiter_solubility_copper = Solubility(
 # copper = Material(D=reiter_diffusivity_copper, S=reiter_solubility_copper, name="copper")
 
 
-magnusson_src = "Self-diffusion and impurity diffusion of hydrogen, oxygen, sulphur and phosphorus in copper, Magnusson & Firsk 2013"
 magnusson_diffusivity_copper = ArrheniusProperty(
     pre_exp=1.74e-6,
     act_energy=42000 * k_B / Rg,
     range=(298, 1273),
-    source=magnusson_src,
+    source="magnusson_self-diffusion_2013",
     name="H Magnusson (2013)",
     isotope="H",
     year=2013,
@@ -50,7 +48,7 @@ magnusson_diffusivity_copper = ArrheniusProperty(
 
 # ################# KATZ 1971 #############################
 
-katz_src = "Diffusion of H2, D2, and T2 in Single-Crystal Ni and Cu, PHYSICAL REVIEW B, VOLUM E 4, NUMBER 2, 15 JULY 1971"
+katz_src = "katz_diffusion_1971"
 data_diffusivity_katz = np.genfromtxt(
     str(Path(__file__).parent) + "/katz_1971_diffusivity.csv",
     delimiter=",",
@@ -96,7 +94,7 @@ eichenauer_diffusivity_copper_h = ArrheniusProperty(
     1.1e-6,
     0.4,
     range=(700, 920),
-    source=cited_in_database_for_ITER,
+    source="eichenauer_notitle_1957",
     name="H Eichenauer (1957)",
     year=1957,
     isotope="H",
@@ -107,7 +105,7 @@ eichenauer_solubility_copper_h = Solubility(
     pre_exp=4.9e23,
     act_energy=0.37,
     range=(700, 920),
-    source=cited_in_database_for_ITER,
+    source="eichenauer_notitle_1957",
     name="H Eichenauer (1957)",
     year=1957,
     isotope="H",
@@ -120,19 +118,20 @@ perkins_diffusivity_copper_h = ArrheniusProperty(
     1.06e-6,
     0.4,
     range=(410, 710),
-    source=cited_in_database_for_ITER,
-    name="H Perkins (1957)",
-    year=1957,
+    source="perkins_permeation_1973",
+    name="H Perkins (1973)",
+    year=1973,
     isotope="H",
     author="perkins",
 )
 
-# ################# Perkins 1973 #############################
+# ################# Tanabe 1987 #############################
+# from Assessment of Database for Interaction of Tritium with ITER Plasma Facing Materials
 tanabe_diffusivity_copper_d = ArrheniusProperty(
     6.7e-8,
     0.24,
     range=(298, 1070),
-    source=cited_in_database_for_ITER,
+    source="tanabe_hydrogen_1987",
     name="D Tanabe (1987)",
     year=1987,
     isotope="D",
@@ -144,7 +143,7 @@ eichenauer_diffusivity_copper_d = ArrheniusProperty(
     6.19e-7,
     0.392,
     range=(703, 913),
-    source="Z. Metallkd., 56 (1965), p. 287",
+    source="eichenauer_notitle_1965",
     name="D Eichenauer (1965)",
     year=1965,
     isotope="D",
@@ -155,7 +154,7 @@ eichenauer_solubility_copper_d = Solubility(
     pre_exp=3.19e24,
     act_energy=0.41,
     range=(703, 913),
-    source="Z. Metallkd., 56 (1965), p. 287",
+    source="eichenauer_notitle_1965",
     name="D Eichenauer (1965)",
     year=1965,
     isotope="D",
@@ -168,7 +167,7 @@ anderl_diffusivity_copper_d = ArrheniusProperty(
     9.26e-7,
     0.409,
     range=(638, 723),
-    source="Hydrogen transport behavior of metal coatings for plasma-facing components, Journal of Nuclear Materials, Volumes 176–177, 3 December 1990, Pages 683-689",
+    source="anderl_hydrogen_1990",
     name="D Anderl (1990)",
     year=1990,
     isotope="D",
@@ -181,7 +180,7 @@ anderl_diffusivity_copper_d_1999 = ArrheniusProperty(
     2.1e-6,
     0.52,
     range=(700, 800),
-    source="Deuterium transport in Cu, CuCrZr, and Cu/Be, Journal of Nuclear Materials 266-269 (1999) 761-765",
+    source="anderl_deuterium_1999",
     name="D Anderl (1999)",
     year=1999,
     isotope="D",
@@ -194,7 +193,7 @@ sakamoto_diffusivity_copper_h = ArrheniusProperty(
     3.69e-7,
     36820 * k_B / Rg,
     range=(292, 339),
-    source="The Electrochemical Determination of  Diffusivity and Solubility of Hydrogen in Copper, J.Japan Inst.Metals, Vol.46, No.3(1982), pp. 285-290",
+    source="sakamoto_electrochemical_1982",
     name="H Sakamoto (1982)",
     year=1982,
     isotope="H",
@@ -206,7 +205,7 @@ otsuka_diffusivity_copper_t = ArrheniusProperty(
     1.11e-6,
     0.399,
     range=(303, 353),
-    source="Behavior of tritium accumulated on materials surface, Fusion Engineering and Design, Volume 85, Issues 7–9, December 2010, Pages 1437–1441",
+    source="otsuka_behavior_2010",
     name="T Otsuka (1982)",
     year=1982,
     isotope="T",
@@ -218,7 +217,7 @@ thomas_solubility_copper_h = Solubility(
     pre_exp=1.90e24,
     act_energy=0.51,
     range=(770, 1320),
-    source=cited_in_database_for_ITER,
+    source="thomas_solubility_1967",
     name="H Thomas (1967)",
     year=1967,
     isotope="H",
@@ -231,7 +230,7 @@ wampler_solubility_copper_h = Solubility(
     pre_exp=1.07e24,
     act_energy=0.44,
     range=(770, 1070),
-    source=cited_in_database_for_ITER,
+    source="wampler_precipitation_1976",
     name="H Wampler (1976)",
     year=1976,
     isotope="H",
