e376f5d6dadaa6c80ed6b241cd446447f94d01a2
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-10-02 11:59:40 +0200

    added tests

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -69,3 +69,23 @@ def test_Torr_to_Pa_reg():
     value = 13.5
 
     assert np.isclose(c.Torr_to_Pa(c.Pa_to_Torr(value)), value)
+
+
+def test_cmHg_conversion():
+    value = 12
+
+    assert c.cmHg_to_Pa(value) == 1333.22 * value
+
+
+def test_ccSTP_to_mol():
+    P = 101.35e3  # Pa
+    T = 273.15  # K
+    R = htm.Rg
+
+    V_cm3 = 1.5  # cm3
+    V_m3 = V_cm3 * 1e-6  # m3
+
+    expected_n = P * V_m3 / (R * T)
+    print(expected_n)
+
+    assert expected_n == c.ccSTP_to_mol(V_cm3)
