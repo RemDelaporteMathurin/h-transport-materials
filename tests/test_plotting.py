9d20d6c231d8440cc53eb4601d70d70a94face7c
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-10-11 09:54:59 +0200

    additional test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -8,6 +8,12 @@ def test_plot():
     htm.plotting.plot(my_prop, alpha=0.2)
 
 
+def test_plot_without_inverse_temp():
+    my_prop = htm.ArrheniusProperty(1, 0.2)
+
+    htm.plotting.plot(my_prop, alpha=0.2, inverse_temperature=False)
+
+
 def test_plot_group():
     """Tests that a group can be plotted"""
 
