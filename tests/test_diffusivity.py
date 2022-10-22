b736b72a3040eed681a4357871ecb89fad8753f3
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-10-22 17:24:36 +0200

    added test for units

--- tests/test_diffusivity.py
+++ tests/test_diffusivity.py
@@ -16,3 +16,35 @@ def test_act_energy_can_be_quantity():
         E_D=0.5 * htm.ureg.kJ * htm.ureg.mol**-1,
     )
     assert prop.act_energy != 0.5
+
+
+def test_fit_with_units_data_y():
+    """Checks that units are taken into account in data_y"""
+    my_prop_ref = htm.Diffusivity(
+        data_T=[1, 2, 3] * htm.ureg.K,
+        data_y=[1, 2, 3] * htm.ureg.m**2 * htm.ureg.s**-1,
+    )
+
+    my_prop_other_units = htm.Diffusivity(
+        data_T=[1, 2, 3] * htm.ureg.K,
+        data_y=[1, 2, 3] * htm.ureg.cm**2 * htm.ureg.s**-1,
+    )
+
+    assert my_prop_other_units.pre_exp != my_prop_ref.pre_exp
+    assert my_prop_other_units.act_energy != my_prop_ref.act_energy
+
+
+def test_fit_with_units_data_T():
+    """Checks that units are taken into account in data_T"""
+    my_prop_ref = htm.Diffusivity(
+        data_T=[1, 2, 3] * htm.ureg.K,
+        data_y=[1, 2, 3] * htm.ureg.m**2 * htm.ureg.s**-1,
+    )
+
+    my_prop_other_units = htm.Diffusivity(
+        data_T=htm.ureg.Quantity([1, 2, 3], htm.ureg.degC),
+        data_y=[1, 2, 3] * htm.ureg.m**2 * htm.ureg.s**-1,
+    )
+
+    assert my_prop_other_units.pre_exp != my_prop_ref.pre_exp
+    assert my_prop_other_units.act_energy != my_prop_ref.act_energy
