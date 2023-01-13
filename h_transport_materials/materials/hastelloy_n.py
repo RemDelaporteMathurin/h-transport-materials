2ed33d1c1e5c50e5b89e10159884cf460aa61c97
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-01-13 14:49:04 -0500

    added zhang data

--- h_transport_materials/materials/hastelloy_n.py
+++ h_transport_materials/materials/hastelloy_n.py
@@ -7,22 +7,83 @@ from h_transport_materials import (
     RecombinationCoeff,
 )
 
+u = htm.ureg
 
 webb_permeability = Permeability(
     pre_exp=htm.conversion.ccSTP_to_mol(190)
-    * htm.ureg.mol
-    * htm.ureg.mm
-    * htm.ureg.cm**-2
-    * htm.ureg.h**-1
-    * htm.ureg.atm**-0.5,
-    act_energy=13800 * htm.ureg.cal * htm.ureg.mol**-1,
+    * u.mol
+    * u.mm
+    * u.cm**-2
+    * u.h**-1
+    * u.atm**-0.5,
+    act_energy=13800 * u.cal * u.mol**-1,
     isotope="H",
-    range=(1 / 0.0015 * htm.ureg.K, 1 / 0.0014 * htm.ureg.K),
+    range=(1 / 0.0015 * u.K, 1 / 0.0014 * u.K),
     source="webb_permeation_1965",
     note="Table 1",
 )
 
-properties = [webb_permeability]
+
+# TODO fit these ourselves
+zhang_permeability_h = Permeability(
+    pre_exp=2.5e-7 * u.mol * u.m**-1 * u.Pa**-0.5 * u.s**-1,
+    act_energy=60.27 * u.kJ * u.mol**-1,
+    range=(u.Quantity(400, u.degC), u.Quantity(800, u.degC)),
+    isotope="H",
+    source="zhang_diffusion_2020",
+)
+
+zhang_permeability_d = Permeability(
+    pre_exp=1.9e-7 * u.mol * u.m**-1 * u.Pa**-0.5 * u.s**-1,
+    act_energy=60.61 * u.kJ * u.mol**-1,
+    range=(u.Quantity(400, u.degC), u.Quantity(800, u.degC)),
+    isotope="D",
+    source="zhang_diffusion_2020",
+)
+
+zhang_diffusivity_h = Diffusivity(
+    D_0=7.0e-7 * u.m**2 * u.s**-1,
+    E_D=44.11 * u.kJ * u.mol**-1,
+    range=(u.Quantity(400, u.degC), u.Quantity(800, u.degC)),
+    isotope="H",
+    source="zhang_diffusion_2020",
+)
+
+zhang_diffusivity_d = Diffusivity(
+    D_0=6.1e-7 * u.m**2 * u.s**-1,
+    E_D=43.70 * u.kJ * u.mol**-1,
+    range=(u.Quantity(400, u.degC), u.Quantity(800, u.degC)),
+    isotope="D",
+    source="zhang_diffusion_2020",
+)
+
+zhang_solubility_h = Solubility(
+    units="m-3 Pa-1/2",
+    S_0=0.36e-7 * u.mol * u.m**-3 * u.Pa**-0.5,
+    E_S=17.60 * u.kJ * u.mol**-1,
+    range=(u.Quantity(400, u.degC), u.Quantity(800, u.degC)),
+    isotope="H",
+    source="zhang_diffusion_2020",
+)
+
+zhang_solubility_d = Solubility(
+    units="m-3 Pa-1/2",
+    S_0=0.31e-7 * u.mol * u.m**-3 * u.Pa**-0.5,
+    E_S=17.81 * u.kJ * u.mol**-1,
+    range=(u.Quantity(400, u.degC), u.Quantity(800, u.degC)),
+    isotope="D",
+    source="zhang_diffusion_2020",
+)
+
+properties = [
+    webb_permeability,
+    zhang_permeability_h,
+    zhang_permeability_d,
+    zhang_diffusivity_h,
+    zhang_diffusivity_d,
+    zhang_solubility_h,
+    zhang_solubility_d,
+]
 
 for prop in properties:
     prop.material = "hastelloy_n"
