32369bfa1dc16d32ddb26454bf33c3e53e4a0995
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2023-05-03 11:17:26 +0200

    add `first_example.py`

---
+++ examples/first_example.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+import h_transport_materials as htm
+
+tungsten_permeabilities = htm.permeabilities.filter(material=htm.TUNGSTEN)
+htm.plotting.plot(tungsten_permeabilities)
+plt.yscale("log")
