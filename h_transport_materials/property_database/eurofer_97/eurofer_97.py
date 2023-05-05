973df70d33456095d860da0ebe845d611c1f208f
J Dark <james.dark.95@gmail.com>
2023-05-05 17:20:37 +0200

    added data fitting for montupet leblond

--- h_transport_materials/property_database/eurofer_97/eurofer_97.py
+++ h_transport_materials/property_database/eurofer_97/eurofer_97.py
@@ -11,11 +11,31 @@ esteban_permeability_data = np.genfromtxt(
     delimiter=",",
 )
 
-esteban_permeability_data_y = esteban_permeability_data[:, 1] 
-
 esteban_permeability = Permeability(
     data_T=1000 / esteban_permeability_data[:, 0] * u.K,
     data_y=esteban_permeability_data[:, 1] * u.mol * u.m**-1 * u.Pa**-0.5 * u.s**-1,
     source="esteban_hydrogen_2007",
     isotope="H",
 )
+
+montupet_leblond_permeability_data = np.genfromtxt(
+    str(Path(__file__).parent) + "/montupet_leblond_2021/permeability.csv",
+    delimiter=",",
+)
+
+montupet_leblond_permeability = Permeability(
+    data_T=1000 / montupet_leblond_permeability_data[:, 0] * u.K,
+    data_y=montupet_leblond_permeability_data[:, 1] * u.mol * u.m**-1 * u.Pa**-0.5 * u.s**-1,
+    source="montupet_leblond_permeation_2021",
+    isotope="H",
+)
+
+properties = [
+    esteban_permeability,
+    montupet_leblond_permeability,
+]
+
+for prop in properties:
+    prop.material = htm.EUROFER
+
+htm.database += properties
