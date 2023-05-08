1bd41ce9a75191d266e8a17c24c0d49923c8ac31
J Dark <james.dark.95@gmail.com>
2023-05-08 19:03:14 +0200

    updated alumina

--- h_transport_materials/property_database/alumina/alumina.py
+++ h_transport_materials/property_database/alumina/alumina.py
@@ -7,6 +7,7 @@ from h_transport_materials import (
 from pathlib import Path
 import numpy as np
 
+u = htm.ureg
 
 serra_data = np.genfromtxt(
     str(Path(__file__).parent) + "/serra_2005/data.csv",
@@ -15,26 +16,22 @@ serra_data = np.genfromtxt(
 )
 
 serra_permeability = Permeability(
-    data_T=1 / serra_data["permx"] * htm.ureg.K,
-    data_y=serra_data["permy"]
-    * htm.ureg.mol
-    * htm.ureg.m**-1
-    * htm.ureg.s**-1
-    * htm.ureg.Pa**-0.5,
+    data_T=1 / serra_data["permx"] * u.K,
+    data_y=serra_data["permy"] * u.mol * u.m**-1 * u.s**-1 * u.Pa**-0.5,
     isotope="H",
     source="serra_hydrogen_2004",
 )
 
 serra_diffusivity = Diffusivity(
-    data_T=1 / serra_data["diffx"] * htm.ureg.K,
-    data_y=serra_data["diffy"] * htm.ureg.m**2 * htm.ureg.s**-1,
+    data_T=1 / serra_data["diffx"] * u.K,
+    data_y=serra_data["diffy"] * u.m**2 * u.s**-1,
     isotope="H",
     source="serra_hydrogen_2004",
 )
 
 serra_solubility = Solubility(
-    data_T=1 / serra_data["solx"] * htm.ureg.K,
-    data_y=serra_data["soly"] * htm.ureg.mol * htm.ureg.m**-3 * htm.ureg.Pa**-0.5,
+    data_T=1 / serra_data["solx"] * u.K,
+    data_y=serra_data["soly"] * u.mol * u.m**-3 * u.Pa**-0.5,
     isotope="H",
     source="serra_hydrogen_2004",
 )
@@ -47,16 +44,16 @@ belonshko_data = np.genfromtxt(
 )
 
 belonshko_diffusivity_solid = Diffusivity(
-    data_T=1 / belonshko_data["alpha_aluminax"] * htm.ureg.K,
-    data_y=belonshko_data["alpha_aluminay"] * htm.ureg.m**2 * htm.ureg.s**-1,
+    data_T=1 / belonshko_data["alpha_aluminax"] * u.K,
+    data_y=belonshko_data["alpha_aluminay"] * u.m**2 * u.s**-1,
     isotope="H",
     source="belonoshko_first-principles_2004",
     note="solid alpha alumina",
 )
 
 belonshko_diffusivity_liquid = Diffusivity(
-    data_T=1 / belonshko_data["liquid_aluminax"] * htm.ureg.K,
-    data_y=belonshko_data["liquid_aluminay"] * htm.ureg.m**2 * htm.ureg.s**-1,
+    data_T=1 / belonshko_data["liquid_aluminax"] * u.K,
+    data_y=belonshko_data["liquid_aluminay"] * u.m**2 * u.s**-1,
     isotope="H",
     source="belonoshko_first-principles_2004",
     note="liquid alumina",
@@ -70,32 +67,32 @@ fowler_data = np.genfromtxt(
 )
 
 fowler_diffusivity_single_crystal = Diffusivity(
-    data_T=1 / fowler_data["single_crystalX"] * htm.ureg.K,
-    data_y=fowler_data["single_crystalY"] * htm.ureg.cm**2 * htm.ureg.s**-1,
+    data_T=1 / fowler_data["single_crystalX"] * u.K,
+    data_y=fowler_data["single_crystalY"] * u.cm**2 * u.s**-1,
     isotope="T",
     source="fowler_tritium_1977",
     note="single crystal",
 )
 
 fowler_diffusivity_powder = Diffusivity(
-    data_T=1 / fowler_data["powderX"] * htm.ureg.K,
-    data_y=fowler_data["powderY"] * htm.ureg.cm**2 * htm.ureg.s**-1,
+    data_T=1 / fowler_data["powderX"] * u.K,
+    data_y=fowler_data["powderY"] * u.cm**2 * u.s**-1,
     isotope="T",
     source="fowler_tritium_1977",
     note="powder",
 )
 
 fowler_diffusivity_sintered = Diffusivity(
-    data_T=1 / fowler_data["sinteredX"] * htm.ureg.K,
-    data_y=fowler_data["sinteredY"] * htm.ureg.cm**2 * htm.ureg.s**-1,
+    data_T=1 / fowler_data["sinteredX"] * u.K,
+    data_y=fowler_data["sinteredY"] * u.cm**2 * u.s**-1,
     isotope="T",
     source="fowler_tritium_1977",
     note="powder",
 )
 
 fowler_diffusivity_doped = Diffusivity(
-    data_T=1 / fowler_data["mgOdopedX"] * htm.ureg.K,
-    data_y=fowler_data["mgOdopedY"] * htm.ureg.cm**2 * htm.ureg.s**-1,
+    data_T=1 / fowler_data["mgOdopedX"] * u.K,
+    data_y=fowler_data["mgOdopedY"] * u.cm**2 * u.s**-1,
     isotope="T",
     source="fowler_tritium_1977",
     note="MgO doped",
