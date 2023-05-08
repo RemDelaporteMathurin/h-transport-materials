66a3760d3d02b95da9fd6f00a115859118bd93b5
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-05-08 15:27:45 -0400

    changed name to helpers

--- h_transport_materials/__init__.py
+++ h_transport_materials/__init__.py
@@ -40,7 +40,7 @@ from .properties_group import PropertiesGroup
 from . import conversion
 from . import fitting
 from . import plotting
-from .import_csv import *
+from .helpers import *
 from .material import *
 
 database = PropertiesGroup()
