71de7c7e9bef11f76b731f113816c2f6120f2c09
J Dark <james.dark.95@gmail.com>
2023-05-08 19:24:01 +0200

    updated berylium

--- h_transport_materials/property_database/beryllium.py
+++ h_transport_materials/property_database/beryllium.py
@@ -1,14 +1,14 @@
 import h_transport_materials as htm
 from h_transport_materials import Diffusivity, Solubility
-
+from h_transport_materials.property import RecombinationCoeff
 import numpy as np
 
-from h_transport_materials.property import RecombinationCoeff
+u = htm.ureg
 
 abramov_diffusivity = Diffusivity(
-    D_0=8.0e-9 * htm.ureg.m**2 * htm.ureg.s**-1,
-    E_D=35.1 * htm.ureg.kJ * htm.ureg.mol**-1,
-    range=(620 * htm.ureg.K, 775 * htm.ureg.K),
+    D_0=8.0e-9 * u.m**2 * u.s**-1,
+    E_D=35.1 * u.kJ * u.mol**-1,
+    range=(620 * u.K, 775 * u.K),
     isotope="D",
     source="abramov_deuterium_1990",
 )
@@ -16,9 +16,9 @@ abramov_diffusivity = Diffusivity(
 
 shapovalov_solubility = Solubility(
     isotope="H",
-    S_0=1.90e-2 * htm.ureg.mol * htm.ureg.m**-3 * htm.ureg.Pa**-0.5,
-    E_S=16.8 * htm.ureg.kJ * htm.ureg.mol**-1,
-    range=(673 * htm.ureg.K, 1473 * htm.ureg.K),
+    S_0=1.90e-2 * u.mol * u.m**-3 * u.Pa**-0.5,
+    E_S=16.8 * u.kJ * u.mol**-1,
+    range=(673 * u.K, 1473 * u.K),
     author="shapovalov",
     source="Shapovalov, V.I., Dukel'skii, Y.M., 1988. Izv. Akad. Nauk SSR Met. 5, 201–203",
     year=1988,
@@ -27,16 +27,16 @@ shapovalov_solubility = Solubility(
 
 jones_diffusivity = Diffusivity(
     isotope="T",
-    D_0=np.exp(-6.53) * htm.ureg.cm**2 * htm.ureg.s**-1,
-    E_D=965 * htm.ureg.K * htm.k_B,
-    range=(400 * htm.ureg.K, 900 * htm.ureg.K),
+    D_0=np.exp(-6.53) * u.cm**2 * u.s**-1,
+    E_D=965 * u.K * htm.k_B,
+    range=(400 * u.K, 900 * u.K),
     source="jones_hydrogen_1967",
     note="Jones also gives a solubility but the units are weird",
 )
 
 dolan_recombination = RecombinationCoeff(
-    pre_exp=1.46e-29 * htm.ureg.m**4 * htm.ureg.s**-1 * htm.ureg.particle**-1,
-    act_energy=0.214 * htm.ureg.eV * htm.ureg.particle**-1,
+    pre_exp=1.46e-29 * u.m**4 * u.s**-1 * u.particle**-1,
+    act_energy=0.214 * u.eV * u.particle**-1,
     source="dolan_assessment_1994",
     isotope="H",
 )
