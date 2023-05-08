67a31696932db51715cd0d3543c531152ea4a722
J Dark <james.dark.95@gmail.com>
2023-05-08 19:26:45 +0200

    updated titanium

--- h_transport_materials/property_database/titanium.py
+++ h_transport_materials/property_database/titanium.py
@@ -1,26 +1,22 @@
 import h_transport_materials as htm
 from h_transport_materials import Diffusivity, Solubility
-import h_transport_materials.conversion as c
 
+u = htm.ureg
 
 TITANIUM_MOLAR_VOLUME = 1.05e-5  # m3/mol https://www.aqua-calc.com/calculate/mole-to-volume-and-weight/substance/titanium
 
 reiter_diffusivity = Diffusivity(
-    D_0=6.9e-7 * htm.ureg.m**2 * htm.ureg.s**-1,
-    E_D=49.1 * htm.ureg.kJ * htm.ureg.mol**-1,
+    D_0=6.9e-7 * u.m**2 * u.s**-1,
+    E_D=49.1 * u.kJ * u.mol**-1,
     source="reiter_compilation_1996",
     isotope="T",
-    range=(873 * htm.ureg.K, 1123 * htm.ureg.K),
+    range=(873 * u.K, 1123 * u.K),
 )
 
 reiter_solubility = Solubility(
-    S_0=1.06e-5
-    / TITANIUM_MOLAR_VOLUME
-    * htm.ureg.mol
-    * htm.ureg.m**-3
-    * htm.ureg.Pa**-0.5,
-    E_S=-42.7 * htm.ureg.kJ * htm.ureg.mol**-1,
-    range=(873 * htm.ureg.K, 1123 * htm.ureg.K),
+    S_0=1.06e-5 / TITANIUM_MOLAR_VOLUME * u.mol * u.m**-3 * u.Pa**-0.5,
+    E_S=-42.7 * u.kJ * u.mol**-1,
+    range=(873 * u.K, 1123 * u.K),
     isotope="T",
     source="reiter_compilation_1996",
 )
