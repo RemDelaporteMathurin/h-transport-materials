8909bea44458f1e345c271228332dd6b06cefc0b
J Dark <james.dark.95@gmail.com>
2023-05-08 19:26:58 +0200

    updated vanadium

--- h_transport_materials/property_database/vanadium_alloy.py
+++ h_transport_materials/property_database/vanadium_alloy.py
@@ -1,22 +1,23 @@
 import h_transport_materials as htm
 from h_transport_materials import Diffusivity, Solubility
 
+u = htm.ureg
 
 hashizume_diffusivity = Diffusivity(
-    D_0=7.50e-4 * htm.ureg.cm**2 * htm.ureg.s**-1,
-    E_D=0.13 * htm.ureg.eV * htm.ureg.particle**-1,
-    range=(373 * htm.ureg.K, 573 * htm.ureg.K),
+    D_0=7.50e-4 * u.cm**2 * u.s**-1,
+    E_D=0.13 * u.eV * u.particle**-1,
+    range=(373 * u.K, 573 * u.K),
     isotope="T",
     source="hashizume_diffusional_2007",
     note="there is a conversion mistake for D_0 in Shimada 2020 review",
 )
 
 klepikov_solubility = Solubility(
-    data_T=[673.0, 773.0, 873.0, 973.0, 1073.0] * htm.ureg.K,
+    data_T=[673.0, 773.0, 873.0, 973.0, 1073.0] * u.K,
     data_y=[1.62e20, 9.84e19, 5.65e19, 4.91e19, 2.94e19]
-    * htm.ureg.particle
-    * htm.ureg.m**-3
-    * htm.ureg.Pa**-0.5,
+    * u.particle
+    * u.m**-3
+    * u.Pa**-0.5,
     isotope="H",
     source="klepikov_hydrogen_2000",
     note="taken from Table 2",
