614b1f45a65f24c62891b89fa0cd97a1b0b8e6bb
natethegreatINL <133147320+natethegreatINL@users.noreply.github.com>
2023-06-23 13:19:59 -0600

    Update __init__.py

--- h_transport_materials/property_database/__init__.py
+++ h_transport_materials/property_database/__init__.py
@@ -62,3 +62,12 @@ from . import nimonic_80A
 from . import incoloy_800
 
 from . import vanadium_alloy
+
+from . import apmt
+from . import fe22cr5al
+from . import thermacore
+from . import thermacore_ox
+from . import t54y
+from . import t35y
+from . import ox_1605
+
