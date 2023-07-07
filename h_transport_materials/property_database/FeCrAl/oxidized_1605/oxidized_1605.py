fix units

h_transport_materials/property_database/FeCrAl/oxidized_1605/oxidized_1605.py
@@ -13,8 +13,7 @@ data = np.genfromtxt(
 
 ash_perm = Permeability(
     data_T=(1000 / data[:,0]) * u.K,
-    #this data is given in micro mol: convert to mol
-    data_y=data[:,1] *1e-6 * u.mol * u.m**(-1) * u.s**(-1) * u.kPa**(-.5),
+    data_y=data[:,1] * u.micromol * u.m**(-1) * u.s**(-1) * u.kPa**(-.5),
     source="ASHDOWN_1980",
     isotope="H",
 )