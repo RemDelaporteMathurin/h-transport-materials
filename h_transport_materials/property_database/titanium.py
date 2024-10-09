86cdc888b7e98a6c6805c37054c1109320b6543e
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-10-09 09:49:23 -0400

    added wille data (3 diffusivities)

--- h_transport_materials/property_database/titanium.py
+++ h_transport_materials/property_database/titanium.py
@@ -21,7 +21,56 @@ reiter_solubility = Solubility(
     source="reiter_compilation_1996",
 )
 
-properties = [reiter_diffusivity, reiter_solubility]
+wille_diffusivity_1 = Diffusivity(
+    D_0=0.9e-2 * u.cm**2 * u.s**-1,
+    E_D=12400 * u.cal * u.mol**-1,
+    range=(873 * u.K, 1123 * u.K),
+    source="wille_hydrogen_1981",
+    isotope="H",
+    note=(
+        "Table 3-1, item a, Commercial Pure Ti, alpha phase. "
+        "Range is unknown and not specified in the report, "
+        "assuming same as for Reiter 1996. The report cites "
+        "another paper from 1958 but don't have access to it."
+    ),
+)
+
+wille_diffusivity_2 = Diffusivity(
+    D_0=0.27e-2 * u.cm**2 * u.s**-1,
+    E_D=14200 * u.cal * u.mol**-1,
+    range=(873 * u.K, 1123 * u.K),
+    source="wille_hydrogen_1981",
+    isotope="H",
+    note=(
+        "Table 3-1, item b, Commercial Pure Ti, alpha phase. "
+        "Range is unknown and not specified in the report, "
+        "assuming same as for Reiter 1996. The report cites "
+        "a master thesis from Schleicher, H but don't have access to it."
+    ),
+)
+
+wille_diffusivity_3 = Diffusivity(
+    D_0=0.06e-2 * u.cm**2 * u.s**-1,
+    E_D=14400 * u.cal * u.mol**-1,
+    range=(873 * u.K, 1123 * u.K),
+    source="wille_hydrogen_1981",
+    isotope="H",
+    note=(
+        "Table 3-1, item d, Commercial Pure Ti, alpha phase. "
+        "Range is unknown and not specified in the report, "
+        "assuming same as for Reiter 1996. The report cites "
+        "another report from Covington 1979, but don't have access to it."
+    ),
+)
+
+
+properties = [
+    reiter_diffusivity,
+    reiter_solubility,
+    wille_diffusivity_1,
+    wille_diffusivity_2,
+    wille_diffusivity_3,
+]
 
 for prop in properties:
     prop.material = htm.TITANIUM
